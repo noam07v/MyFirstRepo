80a9eb1629389804c4247444b2db1bb572adf1fe
noam07v <110410719+noam07v@users.noreply.github.com>
2022-08-29 18:25:52 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,111 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+//      Targil 4
+//        int a = 5;
+//        int b = 12+20;
+//        float c = 5.7f;
+
+//     Targil 5
+//        int a = 4;
+//        int b = 5;
+//        int sum = a + b;
+//        int multiply = a * b;
+//        System.out.println(sum);
+//        System.out.println(multiply);
+//
+//      Targil 6
+//        String color = "blue";
+
+//       Targil 7
+//        System.out.println("Give me your name");
+//        String name = s.nextLine();
+//        System.out.println("Hello " + name + " Welcome to Java");
+
+//       Targil 8
+//        int year = 2020;
+//        System.out.println(year+10-30);
+
+//        Targil 9
+//         int a = s.nextInt();
+//         int b = s.nextInt();
+//         int c = s.nextInt();
+//         System.out.println((a+b+c)/3);
+
+//        Targil 10
+//        System.out.println("what is the speed");
+//        int speed = s.nextInt();
+//        if (speed > 120) {
+//            System.out.println("Too fast");
+//        }
+//        else if (speed < 80) {
+//            System.out.println("Too slow");
+//        }
+//        else {
+//            System.out.println("Good driver");
+//        }
+
+//        Targil 11
+//            int TomerAge = s.nextInt();
+//            int DannyAge = s.nextInt();
+//            if (TomerAge > DannyAge) {
+//                System.out.println("Tomer is older");
+//            }
+//            else if (DannyAge > TomerAge) {
+//            System.out.println("DannyAge is older");
+//        }
+//
+//            Targil 12
+//                int num1 = s.nextInt();
+//                int num2 = s.nextInt();
+//                if (num1 == num2) {
+//                    System.out.println("equal");
+//                } else if (num1 < num2) {
+//                    System.out.println(num1);
+//                }
+//                else {
+//                    System.out.println(num2);
+//                }
+
+//                Targil 13
+//                for (int a = 0; a <= 100; a+=3)
+//                    System.out.println(a);
+
+//                Targil 14
+//            int a = s.nextInt();
+//            int b = s.nextInt();
+//            int c = s.nextInt();
+//            if (a < b < c) {
+//                System.out.println(b);
+//            } else if (a > b < c) {
+//                System.out.println(a);
+//            } else if (a < b > c) {
+//                System.out.println(a);
+//            }
+
+
+//                Targil 18
+//        Random random = new Random();
+//        int randNum;
+//        do {
+//            randNum = random.nextInt(21) + 10;
+//            System.out.println(randNum);
+//        } while (randNum != 15);
+
+//            Targil 16
+//            for (int a = 0; a <= 10; a++) {
+//                System.out.println(a);
+//            }
+
+//            Targil 17
+            for (int a = 1; a <= 50; a += 2 ) {
+                System.out.println(a);
+            }
+
+    }
+}
